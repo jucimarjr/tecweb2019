88d320a7ad2432d62e1793f7882968f1e488afef
richardson-souza <richardson.allan.souza@gmail.com>
2019-05-30 16:06:37 -0400

    resolvendo conflitos

--- react/Angelica/src/index.js
+++ react/Angelica/src/index.js
@@ -8,28 +8,20 @@ import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.jsx";
 import AuthLayout from "layouts/Login.jsx";
-<<<<<<< HEAD
 import TaxiLayout from "layouts/Taxi.jsx";
-
-=======
-import AdminLayout from "layouts/Admin.jsx";
->>>>>>> e2be0766dee8ec95722113a7ac552e7483c601d6
+import UserLayout from "layouts/User.jsx";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-<<<<<<< HEAD
       <Route path="/admin" render={props => <AdminLayout {...props} />} /> 
       <Route path="/taxi" render={props => <TaxiLayout {...props} />} /> 
+      <Route path="/login" render={props => <AuthLayout {...props} />} />
+      <Route path="/user" render={props => <UserLayout {...props} />} />
 
       {/*<Route path="/login" render={props => <AuthLayout {...props} />} /> */}
       <Redirect from="/" to="/admin/index" />
-=======
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Route path="/login" render={props => <AuthLayout {...props} />} />
-      {//<Redirect from="/" to="/login/auth" />
-      }
->>>>>>> e2be0766dee8ec95722113a7ac552e7483c601d6
+
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
