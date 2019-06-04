601f65b08024f6c7b91d3580f417c66904eb330c
kidkmon <kid.mon@hotmail.com>
2019-06-04 00:35:14 -0400

    update crud motorista

--- react/Angelica/src/screens/Motorista/ListMotorista.jsx
+++ react/Angelica/src/screens/Motorista/ListMotorista.jsx
@@ -2,172 +2,228 @@ import React from "react";
 
 // reactstrap components
 import {
-  Button,
   Badge,
   Card,
+  Button,
   CardHeader,
   CardFooter,
-  Col,
   DropdownMenu,
   DropdownItem,
   UncontrolledDropdown,
   DropdownToggle,
   Media,
-  Pagination,
   PaginationItem,
   PaginationLink,
   Progress,
   Table,
   Container,
   Row,
-  UncontrolledTooltip
+  Col,
+  FormGroup,
+  UncontrolledTooltip,
+  InputGroupText,
+  InputGroupAddon,
+  InputGroup,
+  Input,
 } from "reactstrap";
 // core components
 
-import axios from 'axios';
 
 import Header from "components/Login/Headers/UserHeader.jsx";
+import Pagination from "components/Admin/Pagination/Pagination.jsx"
+
+const customLabels = {
+
+  first: '<<',
+  last: '>>',
+  previous: '<',
+  next: '>'
+
+};
+
+const customStyle = {
+  cursor: "Pointer"
+
+};
 
 
 class ListMotorista extends React.Component {
 
   constructor() {
     super();
-
-    
     this.state = {
       motoristas: [],
+      pageOfItems: []
     }
+    this.onChangePage = this.onChangePage.bind(this)
+    this.search = this.search.bind(this)
   }
 
   componentDidMount() {
+    fetch('/drivers')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          motoristas: data.data,
+          pageOfItems: []
+        })
+        console.log(data);
+      }
 
-    const requestInfo = {
-      method: 'GET',
-      crossDomain:true,
-      // mode: 'no-cors',
-      mode: 'cors',
-      headers: { 'Content-Type': 'application/json', 'accept':'application/json' }
-    };
-    axios.get(`http://191.252.184.28/drivers`,{ headers: requestInfo})
-    .then(res => {
-      console.log(res)
-      // const persons = res.data;
-      // this.setState({ persons }
-    });
-    // fetch('http://191.252.184.28/drivers').then(results =>  console.log(results)).catch( r => console.log)
-    // fetch('https://randomuser.me/api/?results=10', requestInfo).then(results =>  console.log(results))//.then(data => {
-      // let motoristas = data.results.map((drive) => {
-      //   return (
-      //     <tr>
-      //       <th scope="row">
-      //         <span className="mb-0 text-sm">
-      //           drive.cpf
-      //         </span>
-      //       </th>
-      //       <td>
-      //         <span className="mb-0 text-sm">
-      //           drive.rg
-      //         </span></td>
-      //       <td>
-      //         <span className="mb-0 text-sm">
-      //           drive.nome
-      //         </span>
-      //       </td>
-      //       <td>
-      //         <span className="mb-0 text-sm">
-      //           drive.renach
-      //         </span>
-      //       </td>
-      //       <td>
-      //         <span className="mb-0 text-sm">
-      //           drive.telefone
-      //         </span>
-      //       </td>
-      //       <td>
-      //         <span className="mb-0 text-sm">
-      //           drive.cep
-      //         </span>
-      //       </td>
-      //       <td>
-      //         <span className="mb-0 text-sm">
-      //           drive.rua
-      //         </span>
-      //       </td>
-      //       <td>
-      //         <span className="mb-0 text-sm">
-      //           drive.bairro
-      //         </span>
-      //       </td>
-      //       <td className="text-right">
-      //         <Button
-      //           color="primary"
-      //           href="edit-motorista"
-      //           // onClick={e => e.preventDefault()}
-      //           size="sm"
-      //         >
-      //           Editar
-      //                 </Button>
-      //       </td>
-      //     </tr>
-      //   )
-      // })
-    //   this.setState({motoristas: data});
-    //   console.log("Motoristas", this.state.motoristas);
-    // })
+      )
+      .catch(console.log)
   }
 
+  onChangePage(pageOfItems) {
+    // update state with new page of items
+    this.setState({ pageOfItems: pageOfItems });
+  }
+
+  search(e){
+    console.log(this.cpf)
+  }
 
   render() {
+    const motoristas = this.state.pageOfItems.map((item) =>
+      <tr>
+        <th scope="row">
+          <Media className="align-items-center">
+            <Media>
+              <span className="mb-0 text-sm">
+                {item.cpf}
+              </span>
+            </Media>
+          </Media>
+        </th>
+        <td>{item.nome}</td>
+        <td>{item.rg}</td>
+        <td>{item.renach}</td>
+        <td>{item.bairro}</td>
+        <td>{item.rua}</td>
+        <td>{item.cep}</td>
+        <td>{item.telefone}</td>
+        <td>{item.status}</td>
+        <td>
+
+          {
+            item.status ? (
+              <Badge color="primary" pill>
+                Ativo
+                              </Badge>
+
+            ) : (
+                <Badge color="danger" pill>
+                  Inativo
+                            </Badge>
+              )}
+
+
+
+
+
+        </td>
+        <td className="text-right">
+          <UncontrolledDropdown>
+            <DropdownToggle
+              className="btn-icon-only text-light"
+              href="#pablo"
+              role="button"
+              size="sm"
+              color=""
+              onClick={e => e.preventDefault()}
+            >
+              <i className="fas fa-ellipsis-v" />
+            </DropdownToggle>
+            <DropdownMenu className="dropdown-menu-arrow" right>
+              <DropdownItem
+                href="#pablo"
+                onClick={e => e.preventDefault()}
+              >
+                Editar
+                            </DropdownItem>
+              <DropdownItem
+                href="#pablo"
+                onClick={e => e.preventDefault()}
+              >
+                Excluir
+                            </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </td>
+      </tr>
+    )
+
     return (
       <>
-        {/* <Header /> */}
+     
         <Header />
         {/* Page content */}
-        <Container className="mt-3" fluid>
-          {/* Dark table */}
-          <Row className="mt-5">
+        <Container className="mt--7" fluid>
+          {/* Table */}
+          <Row>
             <div className="col">
               <Card className="shadow">
-                <CardHeader className="bg-transparent border-0">
-                  <Col xs="8">
-                    <h3 className="mb-0">Buscar Motoristas</h3>
+                <CardHeader className="border-0">
+                <Row>
+                  <Col md="3">
+                    <FormGroup>
+                      <h3 className="mb-0">Buscar Motoristas</h3>
+                    </FormGroup>
                   </Col>
-                  <Col className="text-right" xs="12">
-                    <Button
-                      color="primary"
-                      href="add-motorista"
-                      // onClick={e => e.preventDefault()}
-                      size="sm"
+                  <Col md="5">
+                    <FormGroup>
+                      <InputGroup className="input-group-alternative">
+                      <Input placeholder="CPF" type="text" onChange={e => this.cpf = e.target.value}  />
+                      <Button className="btn-icon btn-3" color="primary" type="button" onClick={this.search}>
+                          Procurar
+                        </Button>
+                      </InputGroup>
+                    </FormGroup>
+                    
+                  </Col>
+                  <Col md="4">
+                    <Button className="btn-icon btn-2" 
+                          color="primary" 
+                          type="button" 
+                          className="float-right"
+                          href="/motorista/add-motorista"
                     >
-                      Cadastrar
-                      </Button>
+                          <span className="btn-inner--icon">
+                            <i className="ni ni-fat-add" />
+                          </span>
+                    </Button>
                   </Col>
+                </Row>
                 </CardHeader>
-                <Table
-                  className="align-items-center table-flush"
-                  responsive
-                >
+                <Table className="align-items-center table-flush" responsive>
                   <thead className="thead-light">
                     <tr>
                       <th scope="col">CPF</th>
+                      <th scope="col">NOME</th>
                       <th scope="col">RG</th>
-                      <th scope="col">Nome</th>
-                      <th scope="col">Renach</th>
-                      <th scope="col">Telefone</th>
-                      <th scope="col">Status</th>
+                      <th scope="col">RENACH</th>
+                      <th scope="col">BAIRRO</th>
+                      <th scope="col">RUA</th>
                       <th scope="col">CEP</th>
-                      <th scope="col">Rua</th>
-                      <th scope="col">Bairro</th>
-                      <th scope="col">Editar</th>
+                      <th scope="col">TELEFONE</th>
+                      <th scope="col">STATUS</th>
+                      <th scope="col" className="text-right">AÇÃO</th>
                       <th scope="col" />
                     </tr>
                   </thead>
                   <tbody>
-                    {this.state.motoristas}
+                      {motoristas}
                   </tbody>
                 </Table>
+                <CardFooter>
+                 <Pagination items={this.state.motoristas} 
+                              onChangePage={this.onChangePage} 
+                              customStyle={customElements.style} 
+                              customLabels={customLabels} />
+                </CardFooter>
               </Card>
+             
+
             </div>
           </Row>
         </Container>
